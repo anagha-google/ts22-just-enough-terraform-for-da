8d1131ac59cfa958bc6bd0942276d691b0650598
Rick(Rugui) Chen <rickruguichen@google.com>
2022-10-24 13:27:46 -0400

    additional BQ tables

--- 00-setup/bigquery.tf
+++ 00-setup/bigquery.tf
@@ -12,6 +12,8 @@ resource "null_resource" "import_csv_to_bq" {
   provisioner "local-exec" {
     command = <<-EOT
     bq load --source_format=CSV --allow_quoted_newlines=true --field_delimiter='|' ${local.bq_datamart_ds}.${local.bq_datamart_sample_table} ../01-datasets/${local.bq_datamart_sample_table}.csv Geography:string
+    bq load --autodetect --source_format=CSV --allow_quoted_newlines=true --field_delimiter=',' ${local.bq_datamart_ds}.${local.bq_datamart_churn_score_table} ../01-datasets/${local.bq_datamart_churn_score_table}.csv 
+    bq load --autodetect --source_format=CSV --allow_quoted_newlines=true --field_delimiter=',' ${local.bq_datamart_ds}.${local.bq_datamart_churn_train_table} ../01-datasets/${local.bq_datamart_churn_train_table}.csv 
   EOT
   }
 
