89670d0bad79d93f54feda31f54269ec95a5c283
Anagha Khanolkar <76123576+anagha-google@users.noreply.github.com>
2022-12-21 12:45:42 -0600

    Update bash.tf

--- 00-setup/shelf/bash.tf
+++ 00-setup/shelf/bash.tf
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-/******************************************
-Run bash scripts
- *****************************************/
 
 resource "null_resource" "run_bash_script" {
     provisioner "local-exec" {
@@ -11,18 +7,5 @@ resource "null_resource" "run_bash_script" {
     depends_on = [
         time_sleep.sleep_after_network_and_storage_steps
     ]
-=======
-
-/******************************************
-10. Customize scripts and notebooks
- *****************************************/
- # Copy from templates and replace variables
-
-resource "null_resource" "vai_pipeline_customization" {
-    provisioner "local-exec" {
-        command = "mkdir ../05-pipelines && cp ../04-templates/customer_churn_vai_pipeline_template.json ../05-pipelines/ && sed -i s/YOUR_PROJECT_NBR/${local.project_nbr}/g ../05-pipelines/customer_churn_vai_pipeline_template.json && sed -i s/YOUR_PROJECT_ID/${local.project_id}/g ../05-pipelines/customer_churn_vai_pipeline_template.json && sed -i s/YOUR_GCP_LOCATION/${local.location}/g ../05-pipelines/customer_churn_vai_pipeline_template.json "
-        interpreter = ["bash", "-c"]
-    }
->>>>>>> main
 }
 
